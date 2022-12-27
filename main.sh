7d01140737a756f8fca80061e0cda64204b5ee35
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-27 20:40:43 +0000

    Fix boolean check

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 input="./Example.txt"
 
 compare_lines() {
-    echo "*****"
+    # echo "*****"
     echo "$1"
     echo "$2"
     echo "*****"
@@ -12,6 +12,7 @@ read_input() {
     while read -r line
     do
         if test "$line" != ""; then
+            
             pairs+=("$line")
             
             if test "${#pairs[@]}" -eq 2; then 
@@ -22,4 +23,22 @@ read_input() {
     done < $1
 }
 
+check_order_nums() {
+    echo "> $1"
+    echo "> $2"
+    [ "$1" -le "$2" ]
+    return
+}
+
 read_input "$input"
+
+check_order_nums 4 5
+echo "$?"
+
+check_order_nums -6 -5
+echo "$?"
+
+
+
+check_order_nums 7 5
+echo "$?"
